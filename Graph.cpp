66f2cdb798c69224fb885827c9df1c1d52296dff
sandeep <cvs.sandeep@gmail.com>
2026-04-13 20:12:45 -0700

    Create Graph.cpp

---
+++ Graph.cpp
@@ -0,0 +1,47 @@
+class Graph {
+  private:
+    unordered_map<string, unordered_set<string> > adjList;
+  public:
+    void printGraph(void) {
+      for(auto kvPair = adjList.begin() ; kvpair != adjList.end() ; kvpair++){
+        cout << kvpair->first <<  ":[";
+        for(auto it : kvpair->second) cout << it;
+        cout << "]" << endl;
+      }
+    }
+    bool addVertex(const string& vertex) {
+        // If vertex doesn't exist, create it with empty set
+        if (adjList.find(vertex) == adjList.end()) {
+            adjList[vertex] = unordered_set<string>();
+            return true;
+        }
+        return false; // Vertex already exists
+    }
+    bool addEdge(string vertex1, string vertex2) {
+		    if(adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0 ) {
+		        adjList.at(vertex1).insert(vertex2);
+		        adjList.at(vertex2).insert(vertex1);
+		        return true;
+		    }
+		    return false;
+		}
+    bool removeEdge(string vertex1, string vertex2) {
+         if(adjList.count(vertex1) != 0 && adjList.count(vertex2) != 0 ) {
+            adjList.at(vertex1).erase(vertex2);
+		        adjList.at(vertex2).erase(vertex1);
+		        return true;
+         }
+      return false;
+    }
+    bool removeVertex(string vertex) {
+      if(adjList.count(vertex) != 0) {
+        for( auto i : adjList[vertex]){
+          adjList[i].erase(vertex);
+        }
+        adjList.erase(vertex);
+        return true;
+      }
+      return false;
+    }
+    
+}
